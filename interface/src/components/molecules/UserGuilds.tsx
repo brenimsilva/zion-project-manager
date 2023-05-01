35792ec92d67ad0118ba46a8db7eac547a7d4dd3
Breno Almeida <brenimsilva7@gmail.com>
2023-04-30 22:38:09 -0300

    [Chore]: Guild Selection

--- interface/src/components/molecules/UserGuilds.tsx
+++ interface/src/components/molecules/UserGuilds.tsx
@@ -1,40 +1,23 @@
 import { IDiscordGuild } from "@/services/DiscordService";
-import React from "react";
+import React, { useState } from "react";
+import Guild from "./Guild";
 
 export interface IUserGuildsProps {
   guilds: Array<IDiscordGuild>;
 }
 export default function UserGuilds({ guilds }: IUserGuildsProps) {
+  const [guildsSelected, setGuildsSelected] = useState<Array<string>>([]);
+
+  function addSelectedGuild(id: string) {
+    setGuildsSelected((prev) => {
+      return [...prev, id];
+    });
+  }
   return (
     <div className="grid grid-cols-2 gap-5">
       {guilds.map((guild) => {
         return (
-          <div
-            key={guild.id}
-            className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-          >
-            <div className="grid grid-cols-4">
-              {
-                <div className="col-span-1">
-                  <img
-                    src={guild.image.src}
-                    className="border rounded-lg"
-                    width="50"
-                  />
-                </div>
-              }
-              <div className="col-span-3">
-                <h5 className="">
-                  <strong>Nome: </strong>
-                  {guild.name}
-                </h5>
-                <p>
-                  <strong>id: </strong>
-                  {guild.id}
-                </p>
-              </div>
-            </div>
-          </div>
+          <Guild id={guild.id} imageSrc={guild.image.src} name={guild.name} />
         );
       })}
     </div>
---
+++ interface/src/store/guild-provider.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function GuildProvider() {
+  return <div>guild-provider</div>;
+}
