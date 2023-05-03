44e1bfc40c772cb51f19186875fc4d0b8aa5b6bf
Breno Almeida <brenimsilva7@gmail.com>
2023-05-02 21:31:09 -0300

    [Feature]: Backend and z-db init

---
+++ z-pannel/src/components/molecules/Select.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function Select() {
+  return (
+    <div>
+      <label
+        id="listbox-label"
+        className="block text-sm font-medium leading-6 text-gray-900"
+      >
+        Assigned to
+      </label>
+      <div className="relative mt-2">
+        <button
+          type="button"
+          className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6"
+          aria-haspopup="listbox"
+          aria-expanded="true"
+          aria-labelledby="listbox-label"
+        >
+          <span className="flex items-center">
+            <span className="ml-3 block truncate">Tom Cook</span>
+          </span>
+          <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+            <svg
+              className="h-5 w-5 text-gray-400"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+}
--- z-pannel/src/components/molecules/UserGuilds.tsx
+++ z-pannel/src/components/molecules/UserGuilds.tsx
@@ -1,11 +1,13 @@
 import { IDiscordGuild } from "@/services/DiscordService";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Guild from "./Guild";
+import Select from "./Select";
 
 export interface IUserGuildsProps {
   guilds: Array<IDiscordGuild>;
 }
 export default function UserGuilds({ guilds }: IUserGuildsProps) {
+  const [cols, setCols] = useState<string | number>(1);
   return (
     <div className="">
       <h5 className="p-5">
@@ -13,7 +15,21 @@ export default function UserGuilds({ guilds }: IUserGuildsProps) {
           Servidores: <span className="text-green-500">{guilds.length}</span>
         </strong>
       </h5>
-      <div className="grid grid-cols-4 gap-5">
+      <div className="p-5">
+        <strong>Cols</strong>
+        <select
+          className="data-te-select-init"
+          onChange={(e) => {
+            setCols(e.target.value);
+          }}
+        >
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+      </div>
+      <div className={`grid grid-cols-${cols} gap-5`}>
         {guilds.map((guild) => {
           return (
             <Guild
