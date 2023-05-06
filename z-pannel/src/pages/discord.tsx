fb984eedcebf22221e2f5f5cf85bc35ea2585c1d
Breno Almeida <brenoalmeida.dev@gmail.com>
2023-05-06 19:57:54 -0300

    [Feature]: Modal component

--- z-pannel/src/pages/discord.tsx
+++ z-pannel/src/pages/discord.tsx
@@ -1,10 +1,24 @@
 import DiscordCenter from "@/components/molecules/DiscordCenter";
+import Modal from "@/components/molecules/Modal";
 import Navbar from "@/components/organisms/Navbar";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
 export default function discord() {
+  const [connected, setConnected] = useState<boolean>(false);
+  const route = useRouter();
+  useEffect(() => {
+    if (!connected) {
+      // route.back();
+    } else {
+
+    }
+  }, [connected])
   return (
     <div>
+      {
+        !connected && <Modal text="Clique no botao abaixo para realizar o login com sua conta discord" title="Não autorizado!"/>
+      }
       <Navbar />
       <DiscordCenter />
     </div>
