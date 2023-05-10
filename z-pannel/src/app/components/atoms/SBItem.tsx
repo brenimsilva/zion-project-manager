2b00339b7792d85abdf5ae42e4560d0c874bac3f
Breno Almeida <brenimsilva7@gmail.com>
2023-05-10 19:33:07 -0300

    [Chore]: Creating Sidebar

---
+++ z-pannel/src/app/components/atoms/SBItem.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SBItem() {
+  return <div>SBItem</div>;
+}
---
+++ z-pannel/src/app/components/atoms/SBSearch.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SBSearch() {
+  return <div>SBSearch</div>;
+}
